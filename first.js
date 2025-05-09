cdb25ef364447dc3f2492e0fbf3f34d156d6d6af
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-09 22:14:09 +0530

    second

--- first.js
+++ first.js
@@ -1,4 +1,9 @@
-console.log("hello ", 67434434)
-document.write("<h1>this is js</h1>")
+// console.log("hello ", 67434434)
+// document.write("<h1>this is js</h1>")
 //  alert("this is last warning")
- 
+//  let a=10;
+//  let b=20;
+//  let c=a+b;
+//  console.log(c);
+let a=10;
+document.write(3.14*a*a);
