fed78290e67075df74dafed942a06b353122ff2f
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-10 19:51:03 +0530

    third

--- first.js
+++ first.js
@@ -5,5 +5,23 @@
 //  let b=20;
 //  let c=a+b;
 //  console.log(c);
-let a=10;
-document.write(3.14*a*a);
+// let a=10;
+// document.write(3.14*a*a);
+// var a=10
+// // let a=12 //var and let are keywords
+// a=14
+// console.log(a+1)
+// const n=12
+// n=1
+// console.log(n*2)
+// var a =prompt("enter a")
+// var b= prompt("enter b")
+// console.log(a+b)
+var r = parseInt(prompt("enter radius"))
+
+console.log(3.14*r*r)
+var b= parseInt(prompt("enter number for cube"))
+console.log(b*b*b)
+var a= parseInt(prompt("enter a"))
+var b= parseInt(prompt("enter b"))
+console.log((a+b)*(a+b))
