0d5bd23e8812bf86d3bb250b562d91142721d04c
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-12 20:07:31 +0530

    fourth

---
+++ conditional.js
@@ -0,0 +1,55 @@
+//if then else
+
+//  let a = parseInt(prompt("enter a"))
+// if(a%2==0){
+//     alert("even")
+// }
+// else{
+//     alert("odd")
+// }
+//  let x= parseInt(prompt("enter x"))
+//  let y= parseInt(prompt("enter y"))
+//     if(x>y){
+//         alert("x is greater")
+//     }
+//     else{
+//         alert("y is greater")
+//     }
+
+//ladder if else 
+
+
+// let a =parseInt(prompt("enter a"))
+// if(a==0){
+//     alert("zero")
+// }
+// else if(a>0){
+//     alert("positive")
+// }
+// else {
+//     alert("negative")
+// }
+
+
+//nested if else
+// if(condition){
+//     if(condition){
+//         //code
+//     }
+//     else{
+//         //code
+//     }
+// }
+// else{
+
+// }
+
+//loop
+
+//do while loop
+// let a=1;
+// do{
+//     console.log(a)
+//     a++
+// }while(a<=10)
+
