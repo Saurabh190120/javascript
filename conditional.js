680e5f9c897d475afd5c3add9768d9b591d88a40
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-16 11:23:16 +0530

    loops

--- conditional.js
+++ conditional.js
@@ -53,3 +53,77 @@
 //     a++
 // }while(a<=10)
 
+// let age = parseInt(prompt("enter age"))
+
+// if(age>=18 ){
+//     let  ns= parseInt(prompt("enter 1 indian"))
+//     if(ns==1)   {
+//         alert("eligible for voting")
+//     }
+//    else{
+//         alert("not eligible for voting")
+//     }
+// }    
+// else{
+//     alert("not eligible for voting")
+// }
+// let a=parseInt(prompt("enter a"))
+// do{
+//    if(a==0){
+//         alert("zero")
+//     }  
+//     else{
+//         let a=parseInt(prompt("enter a"))
+//     }  
+// }while(a<0 || a>0)
+// do{
+//     var a = parseInt(prompt("enter a"))
+      
+// }while(a!=0)
+
+
+//while loop
+//syntax
+// while(condition){
+//     //code
+// }
+
+// let a=1;
+//  let t  = parseInt(prompt("enter a"))
+// while(a<=10){
+//     console.log(a*t)
+//     a++
+// }
+
+//for loop
+//syntax
+// for(initialization;condition;increment/decrement){
+//     //code
+// }
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// let b=parseInt(prompt("enter b"))
+// b=b*b
+// for (let a=10;a>=1;a--){
+//     console.log(a*b)
+// }   
+// for(let i=1;i<=15;i++){
+//    if(i%2==0){
+//         console.log(i)
+//     }
+// }
+// let a = parseInt(prompt("enter a"))
+// if(a%2==0){
+//    for(let i=1;i<=10;i++){
+//         console.log(i*a)
+//     }
+// }
+// else{
+//     console.log(a*a*a)
+// }
+// for(let i=1;i<=10;i++){
+//   for(let j=2;j<=3;j++){
+//         console.log(i*j)
+//     }
+// }
