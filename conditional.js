dffbb1eb45399d30484a824b0a924ea2f9d4f93a
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-16 23:12:28 +0530

    may16

--- conditional.js
+++ conditional.js
@@ -127,3 +127,33 @@
 //         console.log(i*j)
 //     }
 // }
+
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=i;j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+
+// for(let i=1;i<=5;i++){
+//     for(let j=5;j>=i;j--){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=5;j++){
+//         if(i==1 || i==5 || j==1 || j==5){
+//             document.write("*")
+//         }
+//         else{
+//             document.write("&nbsp&nbsp")
+//         }
+    
+//     }
+//     document.write("<br>")
+// }
+
---
+++ datatype.js
@@ -0,0 +1,9 @@
+//datatypes
+
+// This file contains the data types used in the application.
+// It includes the definition of the data types and their properties.
+// type of data types
+// The data types are used to define the structure of the data used in the application.
+
+
+
