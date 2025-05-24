c28e1d7838953ccbe29e6eff1c7f18f211996cbd
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-24 20:08:40 +0530

    funtion type

--- funtion.js
+++ funtion.js
@@ -7,14 +7,53 @@
 //     console.log("inside funtion")
 // }
 // Demo()
-function Demo(a) {
-    let c =a
-    if(c%2==0){
-        console.log("even")
-    }
-    else{
-        console.log("odd")
-    }
-}
-let inp= parseInt(prompt("enter number"))
-Demo(inp)
+// function Demo(a) {
+//     let c =a
+//     if(c%2==0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log("odd")
+//     }
+// }
+// let inp= parseInt(prompt("enter number"))
+// Demo(inp)
+
+
+
+//funtion type
+//no return no argument
+// function Demo() {
+//     console.log("inside funtion")
+// }
+// Demo()
+
+
+
+
+//no return with argument   
+// function Demo(a,b) {
+    
+//     console.log(a+b)
+// }
+// let a =parseInt(prompt("enter a"))
+// let b =parseInt(prompt("enter b"))
+// Demo(a,b)
+
+
+
+
+
+//return with argument
+
+// function Demo(a) {
+//     return a+a
+// }
+//1 //  console.log (Demo(10))
+// 2 let c =Demo(10)
+// console.log(c);
+
+
+//return no argument
+
+
