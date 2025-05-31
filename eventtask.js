22cedbbaf95a2145b74a5f97db51d2cb79bb6f14
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-31 18:57:42 +0530

    events

---
+++ eventtask.js
@@ -0,0 +1,28 @@
+function add() {
+    let a=parseInt(prompt("enter a"))
+     let b=parseInt(prompt("enter b"))
+     console.log(a+b)
+    
+}
+// function red() {
+//     let v=document.body
+//     v.style.backgroundColor="red"
+// }
+// function blue() {
+//     let v=document.body
+//     v.style.backgroundColor="blue"
+// }
+
+// function green() {
+//     let v=document.body
+//     v.style.backgroundColor="green"
+// }
+
+// function yellow() {
+//    let v=document.body
+//     v.style.backgroundColor="green"
+// }
+function color(cl){
+document.body.style.backgroundColor=cl
+}
+
