cfdd6111cd2230e6dd3fc384a6f7fc23638b2ebd
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-05-31 18:12:29 +0530

    may31

---
+++ hw.js
@@ -0,0 +1,64 @@
+// 1. Wap to take a input from the user find square of that number then check number is even or odd using function type 1
+//  function Square(){
+//    let a= parseInt(prompt("enter a number"))
+//    console.log(a*a);
+   
+//    if(a%2==0){
+//     console.log("number is even")
+//    }
+//    else{
+//      console.log("number is odd")
+//    }
+//  }
+//  Square()
+
+
+
+// 2. Wap to return sum of all number present in array using type 4
+// let sa=0
+// function sum(){
+//     let arr=[]
+
+//      let s = parseInt(prompt("enter array size"))
+//      for(let i=0;i<s;i++){
+//         arr[i]=parseInt(prompt("enter element"))
+//      }
+//      for(let a of arr){
+//         sa+=a
+//      }
+//      return sa
+// }
+
+//  let c = sum()
+//  console.log(c)
+
+
+// 3. Wap to take input from the user and return the last digit of that number using function type 3
+
+// function Type3(a) {
+//     var v = a%10
+//     return v
+// }
+// let i=parseInt(prompt("Enter number"))
+// let r=Type3(i)
+// console.log(r)
+
+
+
+
+
+// 4. Wap to take a input from the user and check number is divisible by 3 or not if divisible then print the table if not divisible just print that number using function type 
+ 
+// let  ipt = parseInt(prompt("Enter the number"))
+// function name() {
+//     return ipt;
+//  }
+// if(ipt%3==0){
+//     for(let i=1;i<11;i++){
+//         console.log(ipt*i);
+        
+//     }
+// }
+// else{
+//  console.log(name())
+// }
