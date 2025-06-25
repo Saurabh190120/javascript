3f398beed23bd2932e78cba9e95be85de8375eec
Saurabh190120 <saurabhgurjar1901@gmail.com>
2025-06-25 21:28:20 +0530

    login

---
+++ registration.js
@@ -0,0 +1,18 @@
+let user=JSON.parse(localStorage.getItem("user")||'[]');
+
+const userdata=(e)=>{
+    e.preventDefault();
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let mobile=document.getElementById("mobile").value;
+    let password=document.getElementById("password").value;
+    let z={name,email,mobile,password}
+    user.push(z)
+    localStorage.setItem("user",JSON.stringify(user))
+    // console.log(user)
+    window.location.href='/login.html';
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    document.querySelector("form").addEventListener("submit",userdata)
+})
